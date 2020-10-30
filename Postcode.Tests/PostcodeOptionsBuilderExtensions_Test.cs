7fb58875d1a9437cf3e9fa9ffae079a7ba5948a0
omvf <61068260+omvf@users.noreply.github.com>
2020-10-30 20:54:37 +0000

    feat: First test

---
+++ Postcode.Tests/PostcodeOptionsBuilderExtensions_Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Postcode.Tests
+{
+    public class PostcodeOptionsBuilderExtensions_Test
+    {
+        [Test]
+        public void AddPostcodeIO_ShouldException()
+        {
+            
+        }
+    }
+}
